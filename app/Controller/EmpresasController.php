de1823913773da33344c4fba739039439e91e827
Neylor <harakiri.is.honor@gmail.com>
2013-02-05 16:12:56 -0300

    primeiro commit

---
+++ app/Controller/EmpresasController.php
@@ -0,0 +1,9 @@
+﻿<?php
+	class EmpresasController extends AppController {
+		public $helpers = array('Html', 'Form');
+
+		public function index() {
+			$this->set('empresas', $this->Empresa->find('all'));
+		}
+	}
+?>
